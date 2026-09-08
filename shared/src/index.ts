9012722a3d5939ae849a3bbce1172a98cb755218
ahmadkimar78-code <ahmadkimar78@gmail.com>
2026-09-08 03:08:25 +0300

    Update index.ts

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,2 +1,8 @@
-export * from "./orderStatus";
+export {
+  OrderStatus,
+  ORDER_STATUS_LABELS_AR,
+  ALLOWED_TRANSITIONS,
+  isTransitionAllowed,
+} from "./orderStatus";
+export type { OrderStatusType, AppRole } from "./orderStatus";
 export * from "./types";
